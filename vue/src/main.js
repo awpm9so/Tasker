749323ee4691c46cdac1e32a96be36f56443e69c
Savinykh Vladimir <swa@pbprog.ru>
2022-04-20 16:10:41 +0300

    init vue-router

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import store from './store'
+import route from './router'
 import App from './App.vue'
 
 createApp(App)
     .use(store)
+    .use(route)
     .mount('#app')
